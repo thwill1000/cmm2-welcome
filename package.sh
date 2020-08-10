532718a1c5a9184d94b059926060f617277a254f
Thomas Williams <t.williams@psenterprise.com>
2020-08-10 23:01:36 +0100

    Add initial version of 'eliza'
    Lifted from this thread on TBS:
    
      https://www.thebackshed.com/forum/ViewTopic.php?TID=12468

--- package.sh
+++ package.sh
@@ -5,6 +5,7 @@ release_dir="welcome-r$release"
 base="$release_dir/welcome"
 
 mkdir -p $base
+mkdir -p $base/eliza
 mkdir -p $base/launcher
 mkdir -p $base/lunar
 #mkdir -p $base/src
@@ -14,6 +15,7 @@ cp LICENSE $base
 cp README.md $base
 cp welcome.bas $base
 #cp -R resources/* $base/resources
+cp -R eliza/* $base/eliza
 cp -R launcher/* $base/launcher
 cp -R lunar/* $base/lunar
 #cp docs/sptools.pdf $base
